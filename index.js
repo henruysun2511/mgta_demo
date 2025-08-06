d5c594c7ca00e3c2e72bf5f65f1cf1870efff3ba
huysun <huysun2511@gmail.com>
2025-08-06 22:37:45 +0700

    hoan thien giao dien

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#header").load("components/header.html");
     $("#footer").load("components/footer.html");
+    $("#login").load("components/register.html");
 });
